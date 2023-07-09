e5b927962d447f490315e06d743c6721e8441577
rubybo <Kapturalex4@gmail.com>
2023-07-09 17:19:31 +0300

    v001b

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest',
+    'core',
+    'chat',
+    'accounts',
 ]
 
 MIDDLEWARE = [
