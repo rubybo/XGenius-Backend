41a3aa9ba157634089779c271790a8acf4879903
David <113856260+Davidkovel@users.noreply.github.com>
2023-07-11 15:04:41 +0100

    Delete core/static directory

--- core/static/core/js/About-46dad48a.js
+++
@@ -1,1 +0,0 @@
-import{o as a,c as o}from"./index-8254f191.js";const c={name:"App",watch:{$route:{immediate:!0,handler(e,t){document.title=e.meta.title||"XGenius - About"}}}},s=Object.assign(c,{setup(e){return(t,r)=>(a(),o("div"))}});export{s as default};
