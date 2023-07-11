5af6598c8690f9f31b0c4bc4b91ff40792cd8d9e
rubybo <Kapturalex4@gmail.com>
2023-07-11 15:59:10 +0300

    Views

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('chat/', include('chat.urls')),
-    path('accounts/', include('accounts.urls')),
-    path('api/', include('rest.urls'))
+    # path('chat/', include('chat.urls')),
+    # path('accounts/', include('accounts.urls')),
+    # path('api/', include('rest.urls'))
 ]
