beff86179135a3ca5d4f98be032b2cc0bc38affe
David <113856260+Davidkovel@users.noreply.github.com>
2023-07-12 14:34:17 +0100

    regestrations

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from django.views.generic import TemplateView
+from .views import RegisterView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='core/index.html'), name='home')
+    path('', TemplateView.as_view(template_name='core/index.html'), name='home'),
+    path('register/', RegisterView.as_view(), name='register'),
 
 ]
