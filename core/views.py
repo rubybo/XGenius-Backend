ea8873ed4794c1c5abce1e8fec0224fe5b1f5c5e
David <113856260+Davidkovel@users.noreply.github.com>
2023-07-12 14:33:51 +0100

    regestration

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = ...
+    success_url = reverse_lazy('login')
